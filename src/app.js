09f9fea8db92cbe32697dda85fac0a660e6c72c2
ArchanaManju <archanahm2021@gmail.com>
2021-11-23 22:47:36 +0000

    Updated Search

--- src/app.js
+++ src/app.js
@@ -75,3 +75,5 @@ fahrenheitLink.addEventListener('click', displayFahrenheitTemperature);
 
 let celsiusLink = document.querySelector('#celsius-link');
 celsiusLink.addEventListener('click', displayCelsiusTemperature);
+
+search('London');
